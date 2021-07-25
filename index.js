12a9b21ef826bad35e4b9a66a8c148746f0bb13e
Kyle Andruczk <kyle.andruczk@gmail.com>
2021-07-25 15:23:18 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your code here
+function submitData(name, email) {
+
+    let formData = {
+        name: name,
+        email: email
+    }
+
+    let configObj = {
+        method: "POST", 
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(formData)
+    }
+
+
+    let data = fetch('http://localhost:3000/users', configObj)
+        .then( resp => resp.json() )
+        .then( json => document.querySelector('body').textContent = json["id"] )
+        .catch( error => document.querySelector('body').textContent = error.message )
+    return data
+}
